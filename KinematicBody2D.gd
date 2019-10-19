be36dffac744fae9e36e158adb037fea1e1f8e7a
Andrew Siedlak <andrewsiedlak@gmail.com>
2019-10-18 22:45:19 -0400

    Animation change, broke out tank scene
    Redid tank animations. Broke tank entity out as seperate scene from test scene (best practice). Can't figure out idle animation currently.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,20 +1,31 @@
 extends KinematicBody2D
 
-var speed = 250
+var speed = 70
 var velocity = Vector2()
 
 func get_input():
-    # Detect up/down/left/right keystate and only move when pressed.
-    velocity = Vector2()
-    if Input.is_action_pressed('ui_right'):
-        velocity.x += 1
-    if Input.is_action_pressed('ui_left'):
-        velocity.x -= 1
-    if Input.is_action_pressed('ui_down'):
-        velocity.y += 1
-    if Input.is_action_pressed('ui_up'):
-        velocity.y -= 1
-    velocity = velocity.normalized() * speed
+	# Detect up/down/left/right keystate and only move when pressed.
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+		$AnimatedSprite.play("right")
+		$AnimatedSprite.flip_h = false
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+		$AnimatedSprite.play("right")
+		$AnimatedSprite.flip_h = true
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+		$AnimatedSprite.play('up')
+		$AnimatedSprite.flip_v = true
+		print("Down")
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+		$AnimatedSprite.play('up')
+		$AnimatedSprite.flip_v = false
+		print("Up")	
+		
+	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
