150428b198a01118c2c33671d40d325efdd7b07f
andrewsiedlak <andrewsiedlak@gmail.com>
2019-10-19 14:52:38 -0400

    Fixed idle animation
    Fixed issue with not being able to play idle sprites probably.

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,6 +2,13 @@ extends KinematicBody2D
 
 var speed = 70
 var velocity = Vector2()
+<<<<<<< Updated upstream
+=======
+var shot_cooldown_begin = OS.get_ticks_msec()
+var facing_dir = null
+
+const PROJECTILE = preload("res://tank_projectile.tscn")
+>>>>>>> Stashed changes
 
 func get_input():
 	# Detect up/down/left/right keystate and only move when pressed.
@@ -10,20 +17,23 @@ func get_input():
 		velocity.x += 1
 		$AnimatedSprite.play("right")
 		$AnimatedSprite.flip_h = false
-	if Input.is_action_pressed('ui_left'):
+	elif Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
 		$AnimatedSprite.play("right")
-		$AnimatedSprite.flip_h = true
-	if Input.is_action_pressed('ui_down'):
+		$AnimatedSprite.flip_h = true	
+	elif Input.is_action_pressed('ui_down'):
 		velocity.y += 1
 		$AnimatedSprite.play('up')
 		$AnimatedSprite.flip_v = true
-		print("Down")
-	if Input.is_action_pressed('ui_up'):
+	elif Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
 		$AnimatedSprite.play('up')
 		$AnimatedSprite.flip_v = false
-		print("Up")	
+	else:
+		if $AnimatedSprite.animation.get_basename() == "up":
+			$AnimatedSprite.play("idle_up")
+		elif $AnimatedSprite.animation.get_basename() == "right":
+			$AnimatedSprite.play("idle_right")
 		
 	velocity = velocity.normalized() * speed
 
