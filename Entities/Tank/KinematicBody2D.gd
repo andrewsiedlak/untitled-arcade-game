3114d5a8b7dc2e77e33e9c437e9549a454d8df40
NolanWarning <56321097+NolanWarning@users.noreply.github.com>
2020-03-20 14:37:29 -0400

    updated
    added, rapid, multi, and 'fast' shots

--- Entities/Tank/KinematicBody2D.gd
+++ Entities/Tank/KinematicBody2D.gd
@@ -3,12 +3,12 @@ extends KinematicBody2D
 class_name Tank
 
 const PROJECTILE = preload("res://Entities/TankProjectile/tank_projectile.tscn")
-const SHOT_COOLDOWN_MSEC = 300
+var SHOT_COOLDOWN_MSEC = 300
 const SPEED = 70
 
 var velocity = Vector2()
 var shot_cooldown_begin = OS.get_ticks_msec()
-var facing_dir = null
+var facing_dir = 270
 
 func get_input():
 	# Detect up/down/left/right keystate and only move when pressed.
@@ -17,34 +17,73 @@ func get_input():
 		velocity.x += 1
 		$AnimatedSprite.play("moving")
 		self.global_rotation_degrees = 90
-		facing_dir = "right"
+		facing_dir = 360
 	elif Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
 		$AnimatedSprite.play("moving")
 		self.global_rotation_degrees = -90
-		facing_dir = "left"	
+		facing_dir = 180
 	elif Input.is_action_pressed('ui_down'):
 		velocity.y += 1
 		$AnimatedSprite.play("moving")
 		global_rotation_degrees = 180
-		facing_dir = "down"
+		facing_dir = 90
 	elif Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
 		$AnimatedSprite.play('moving')
 		self.global_rotation_degrees = 0
-		facing_dir = "up"
+		facing_dir = 270
 	else:
 		$AnimatedSprite.play("idle")
-		
+	
+	#normal fire
 	if Input.is_key_pressed(KEY_SPACE):
-		
 		if abs(shot_cooldown_begin - OS.get_ticks_msec()) > SHOT_COOLDOWN_MSEC:
 			shot_cooldown_begin = OS.get_ticks_msec()
 			var projectile = PROJECTILE.instance()
 			projectile.set_direction(facing_dir)
 			get_parent().add_child(projectile)
 			projectile.position = $Position2D.global_position
-		
+			shot_cooldown_begin = OS.get_ticks_msec()
+	#Rapid
+	if Input.is_key_pressed(KEY_1):
+		SHOT_COOLDOWN_MSEC = 150
+		if abs(shot_cooldown_begin - OS.get_ticks_msec()) > SHOT_COOLDOWN_MSEC:
+			shot_cooldown_begin = OS.get_ticks_msec()
+			var projectile = PROJECTILE.instance()
+			projectile.set_direction(facing_dir)
+			get_parent().add_child(projectile)
+			projectile.position = $Position2D.global_position
+			shot_cooldown_begin = OS.get_ticks_msec()
+		SHOT_COOLDOWN_MSEC = 300
+	#Rocket
+	if Input.is_key_pressed(KEY_2):
+		if abs(shot_cooldown_begin - OS.get_ticks_msec()) > SHOT_COOLDOWN_MSEC:
+			shot_cooldown_begin = OS.get_ticks_msec()
+			var projectile = PROJECTILE.instance()
+			projectile.SPEED = 240
+			projectile.set_direction(facing_dir)
+			get_parent().add_child(projectile)
+			projectile.position = $Position2D.global_position
+			shot_cooldown_begin = OS.get_ticks_msec()
+	#Multi
+	if Input.is_key_pressed(KEY_3):
+		if abs(shot_cooldown_begin - OS.get_ticks_msec()) > SHOT_COOLDOWN_MSEC:
+			shot_cooldown_begin = OS.get_ticks_msec()
+			var projectile = PROJECTILE.instance()
+			projectile.set_direction(facing_dir-20)
+			get_parent().add_child(projectile)
+			projectile.position = $Position2D.global_position
+			shot_cooldown_begin = OS.get_ticks_msec()
+			var projectile2 = PROJECTILE.instance()
+			projectile2.set_direction(facing_dir)
+			get_parent().add_child(projectile2)
+			projectile2.position = $Position2D.global_position
+			shot_cooldown_begin = OS.get_ticks_msec()
+			var projectile3 = PROJECTILE.instance()
+			projectile3.set_direction(facing_dir+20)
+			get_parent().add_child(projectile3)
+			projectile3.position = $Position2D.global_position
 		
 		
 	velocity = velocity.normalized() * SPEED
--- Entities/TankProjectile/tank_projectile.gd
+++ Entities/TankProjectile/tank_projectile.gd
@@ -1,23 +1,33 @@
 extends Area2D
 
-const SPEED = 120
+var SPEED = 120
 var velocity = Vector2()
-var direction = null
+var direction = "up"
 
 func set_direction(facing_dir):
 	
 	var dir_vector
-	if facing_dir == "right":
-		dir_vector = Vector2(1, 0)
-		$AnimatedSprite.global_rotation_degrees = 90
-	elif facing_dir == "left":
-		dir_vector = Vector2(-1, 0)
-		$AnimatedSprite.global_rotation_degrees = -90
-	elif facing_dir == "up":
-		dir_vector = Vector2(0, -1)
-	elif facing_dir == "down":
-		dir_vector = Vector2(0, 1)
-		$AnimatedSprite.global_rotation_degrees = 180
+	var x_pos = 0
+	var y_pos = 0
+	
+	x_pos = cos(facing_dir * (PI/180))
+	y_pos = sin(facing_dir * (PI/180))
+	
+	dir_vector = Vector2(x_pos, y_pos)
+	$AnimatedSprite.global_rotation_degrees = facing_dir + 90
+	
+	
+#	if facing_dir == 360 or facing_dir == 0:
+#		dir_vector = Vector2(1, 0)
+#		$AnimatedSprite.global_rotation_degrees = 90
+#	elif facing_dir == 180:
+#		dir_vector = Vector2(-1, 0)
+#		$AnimatedSprite.global_rotation_degrees = -90
+#	elif facing_dir == "up":
+#		dir_vector = Vector2(0, -1)
+#	elif facing_dir == "down":
+#		dir_vector = Vector2(0, 1)
+#		$AnimatedSprite.global_rotation_degrees = 180
 				
 	direction = dir_vector
 
