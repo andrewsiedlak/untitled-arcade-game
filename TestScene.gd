8e570c61a12ec0a5f920285ab654a403265e94a7
andrewsiedlak <andrewsiedlak@gmail.com>
2020-04-08 04:42:15 -0400

    Updated player targets to be passed
    Based on new player select logic

--- TestScene.gd
+++ TestScene.gd
@@ -3,21 +3,27 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var Players = [$Lion, $Tiger,$Eagle,$Bear]
-
-var players = [$Player, $Player2, $Player3, $Player4]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
+	var players = []
 	if not globals.player_1_joined:
 		$Bear.queue_free()
+	else:
+		players.append($Bear)
 	if not globals.player_2_joined:
 		$Tiger.queue_free()
+	else:
+		players.append($Tiger)
 	if not globals.player_3_joined:
 		$Eagle.queue_free()
+	else:
+		players.append($Eagle)
 	if not globals.player_4_joined:
 		$Lion.queue_free()
+	else:
+		players.append($Lion)
 	
 	$Boss.targets = $Bear
 	$Boss.shoot()
