e6d7905b57d6c73967fd6a3955dcba837c3726b8
andrewsiedlak <andrewsiedlak@gmail.com>
2020-04-07 20:11:47 -0400

    Changed how boss receives targets

--- TestScene.gd
+++ TestScene.gd
@@ -4,9 +4,11 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var players = [$Player, $Player2, $Player3, $Player4]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Boss.target = $Player
+	$Boss.targets = players
 	$Boss.shoot()
 	pass
 
